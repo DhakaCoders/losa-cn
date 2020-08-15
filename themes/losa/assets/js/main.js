8fd21918803501a1fe52b84526e767669187a2f1
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2020-08-15 13:27:18 +0600

    ..............

--- themes/losa/assets/js/main.js
+++ themes/losa/assets/js/main.js
@@ -223,6 +223,37 @@ if( $('.hmSlider').length ){
     $(this).addClass('hh-accordion-active');
     return false;
   });
+
+
+
+
+
+
+  var $slider = $('.ptMainslider');
+
+  if ($slider.length) {
+    var currentSlide;
+    var slidesCount;
+    var sliderCounter = document.createElement('div');
+    sliderCounter.classList.add('slider__counter');
+
+    var updateSliderCounter = function(slick, currentIndex) {
+      currentSlide = slick.slickCurrentSlide() + 1;
+      slidesCount = slick.slideCount;
+      $(sliderCounter).text(currentSlide + ' de ' +slidesCount)
+    };
+
+    $slider.on('init', function(event, slick) {
+      $slider.append(sliderCounter);
+      updateSliderCounter(slick);
+    });
+
+    $slider.on('afterChange', function(event, slick, currentSlide) {
+      updateSliderCounter(slick, currentSlide);
+    });
+
+    $slider.slick();
+  }
   /* End-of-Noyon*/
 
 
