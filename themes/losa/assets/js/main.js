597d1cc39b63d93bc306fe0661fd563f87ba776f
Rannojit <cserannojitsarkar22@gmail.com>
2020-08-16 13:19:14 +0600

    .

--- themes/losa/assets/js/main.js
+++ themes/losa/assets/js/main.js
@@ -263,8 +263,17 @@ if( $('.hmSlider').length ){
     slidesToScroll: 1,
     arrows: true,
     autoplay: true,
+    dots: true,
+    dotsClass: 'custom_paging',
+    customPaging: function (slider, i) {
+        //FYI just have a look at the object to find available information
+        //press f12 to access the console in most browsers
+        //you could also debug or look in the source
+        console.log(slider);
+        return  (i + 1) + 'de' + slider.slideCount;
+    }
   });
-  $(".ptMainslider").slick("getSlick").slideCount;
+  //$(".ptMainslider").slick("getSlick").slideCount;
   /* End-of-Noyon*/
 
 
